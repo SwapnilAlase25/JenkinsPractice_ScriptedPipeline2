e8dd1eb77a472417e8c60794eff8311c78fc84a5
Swapnil_Alase <swapnilalase07@gmail.com>
2020-09-04 18:10:37 +0530

    Create build_job.groovy

---
+++ jobs/build_job.groovy
@@ -0,0 +1,16 @@
+
+//#!groovy
+
+node {
+        stage('build1') {
+                 echo "Building C files ...."
+              }
+		
+		stage('build2') {
+                 echo "Building C++ files ...."            
+        	}
+		
+		stage('build3') {
+                 echo "Building Java files ...."
+              }
+  	 }
